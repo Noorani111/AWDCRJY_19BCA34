203adbef606c0432b3264e64a65d74bfc6e159ed
Noorani111 <86504786+Noorani111@users.noreply.github.com>
2021-07-05 05:49:00 +0530

    Add files via upload

---
+++ 3 july 2021 automorphic.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String args[]) {
+	 int n;
+	 Scanner s=new Scanner(System.in);
+	 n=s.nextInt();
+	int n1=n*n;
+	int re,rem1;
+	re=n%10;
+	rem1=n1%10;
+	if(re==rem1)
+	{
+	    System.out.println("Automorphic");
+	}
+	else 
+	{
+	   System.out.println("Not Automorphic");  
+	}
+	
+	}
+}
